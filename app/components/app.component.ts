a6d3e00327d4c3f8e45e2647d402c96ff18d0f43
Manoj Nama <manoj.nama@outlook.com>
2016-04-01 15:14:32 +0530

    Adding the basic scripts and a simple app

---
+++ app/components/app.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "angular2/core";
+
+@Component({
+	selector: "app",
+	template: `
+		<h1>Welcome to Angular2 with TypeScript</h1>
+	`
+});
+
+export default class AppComponent {
+	
+}
---
+++ app/main.ts
@@ -0,0 +1,5 @@
+import {bootstrap} from "angular2/platform/browser";
+import AppComponent from "./components/app.component";
+
+//Initialize the app
+bootstrap(AppComponent);
