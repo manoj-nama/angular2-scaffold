b8bc9bfb4cc6563e8d87f3168461166f6fb3cf13
Manoj Nama <manoj.nama@outlook.com>
2016-04-01 16:16:28 +0530

    Adding default styling to the component

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -2,6 +2,9 @@ import {Component} from "angular2/core";
 
 @Component({
 	selector: "app",
-	template: "<h1>Welcome to Angular2 with TypeScript</h1>"
+	template: `
+		<h1>Welcome to Angular2 Scaffold</h1>
+		<p>Quick start for <em>Angular2</em> with <strong>TypeScript</strong></p>
+	`
 }) //<-- No semicolons here, it breaks the TS compiler
 export class AppComponent {}
